94e44adceb6d9a3a4075670e5dfdb4155fe25028
germanmalykh <malykh.gn@esphere.ru>
2023-08-17 09:46:03 +0300

    ParameterContext методы вынесены в Extension классы

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
@@ -6,10 +6,13 @@ import guru.qa.niffler.model.CategoryJson;
 import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
-public class CategoryExtension implements BeforeEachCallback {
+public class CategoryExtension implements BeforeEachCallback, ParameterResolver {
 
     public static ExtensionContext.Namespace NAMESPACE = ExtensionContext.Namespace.create(CategoryExtension.class);
 
@@ -32,5 +35,19 @@ public class CategoryExtension implements BeforeEachCallback {
             CategoryJson createdCategory = categoryService.addCategory(category).execute().body();
             context.getStore(NAMESPACE).put("category", createdCategory);
         }
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter()
+                .getType()
+                .isAssignableFrom(CategoryJson.class);
+    }
+
+    @Override
+    public CategoryJson resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return extensionContext
+                .getStore(CategoryExtension.NAMESPACE)
+                .get("category", CategoryJson.class);
     }
 }
